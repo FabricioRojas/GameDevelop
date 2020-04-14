f35a229f0b5dfd8397c6a9638bb57d0a80fa7043
Fabricio Rojas <fabriciorojascuomo@outlook.es>
2020-04-14 14:13:23 +0100

    Added sounds to flappy

--- frame/src/js/gameFrame.js
+++ frame/src/js/gameFrame.js
@@ -682,8 +682,8 @@ class RectElement extends CanvasElement{
     /* Methods */
     print(){
         if(this.rotate){
-            this.context.save();
-            this.context.translate(this.x, this.y);
+            this.context.save();            
+            this.context.translate(this.x+(this.width/2), this.y+(this.height/2));
             this.context.rotate(this.angle);
             this.context.fillStyle = this.color;
             this.context.fillRect(this.width / -2, this.height / -2, this.width, this.height);
